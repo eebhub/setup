ae5413d29e53c234278800f6db788e1871414dfa
Nicholas Mattise <n.mattise@gmail.com>
2014-01-18 10:42:10 -0500

    new update.sh with dates- need to test

--- update.sh
+++ update.sh
@@ -1,11 +1,7 @@
-#Stop the Existing server
-forever stop
-
 #Move the the htdocs root
 cd ~/
-
-#Remove the old platform
-sudo rm -r platform
+#Rename the old folder
+mv platform "Archive_`date +%B``date +%d``date +%y`"
 
 #git clone in the new one
 git clone https://github.com/eebhub/platform.git
@@ -16,6 +12,14 @@ cd platform
 #update the application if necessary
 sudo npm update
 
+#Move the the htdocs root
+cd ~/
+
+#Stop the Existing server
+forever stop
+
+#move to platform
+cd platform
 
 #Start the app
 forever start app.js
