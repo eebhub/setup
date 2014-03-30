ca2d4eccd8e25090e59e23bfd0711cec93455b48
Josh Wentz <joshwentz@gmail.com>
2014-03-29 21:10:44 -0400

    Update Rename To Move Rather than Remove if a folder doesn't exist

--- update-new.sh
+++ update-new.sh
@@ -19,7 +19,8 @@ forever stop app.js
 cd ~/
 
 # RENAME OLD app folder to archive
-sudo cp -rf openstudio_nodejs-live/* "archive_`date +%Y`-`date +%m`-`date +%d`"/; sudo rm -rf openstudio_nodejs-live;
+sudo mv -f openstudio_nodejs-live "archive_`date +%Y`-`date +%m`-`date +%d`"/; sudo rm -rf openstudio_nodejs-live;
+#sudo cp -rf openstudio_nodejs-live/* "archive_`date +%Y`-`date +%m`-`date +%d`"/; sudo rm -rf openstudio_nodejs-live; #if no existing folder, contents will just be deleted
 # mv openstudio_nodejs-live "archive_`date +%Y`-`date +%m`-`date +%d` #if same day, this will move into existing daily archive & break live
 
 # RENAME NEW app folder to live
